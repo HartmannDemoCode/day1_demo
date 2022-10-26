61080d670109e332e6e0653cfa67aff77a09872b
Thomas Hartmann <thomas@webtrade.dk>
2022-10-26 17:26:25 +0200

    refactored to remove warnings

--- src/BookService.jsx
+++ src/BookService.jsx
@@ -4,42 +4,29 @@ function BookService() {
     const [booksChanged, setBooksChanged] = useState(false);
 
     useEffect(() => {
-        const getData = async () => {
+        (async () => {
             const response = await fetch('http://localhost:4000/books');
             const data = await response.json();
             setBooks(data);
-        }
-        getData();
-        
-            // .then((res) => res.json())
-            // .then(data => {
-            //     setBooks(data);
-            // });
-    },[booksChanged]);
+        })();
+    }, [booksChanged]);
     return (
         <>
-            <BookForm isChanged={setBooksChanged}/>
-            <table>
+            <BookForm isChanged={setBooksChanged} />
+            {books.length && <table>
                 <thead>
                     <tr><th>ID</th><th>TITLE</th><th>AUTHOR</th><th>RATING</th><th>PUBLISHED</th></tr>
                 </thead>
-                <tbody>
-
-                    {books.length && books.map((book) => {
-                        return (
-                            <>
-                                <tr>
-                                    <td>{book.id}</td>
-                                    <td>{book.title}</td>
-                                    <td>{book.author}</td>
-                                    <td>{book.rating}</td>
-                                    <td>{book.year_published}</td>
-                                </tr>
-                            </>
-                        );
-                    })}
-                </tbody>
-            </table>
+                <tbody>{books.map((book) => {
+                        return (<tr key={book.id}>
+                            <td>{book.id}</td>
+                            <td>{book.title}</td>
+                            <td>{book.author}</td>
+                            <td>{book.rating}</td>
+                            <td>{book.year_published}</td>
+                            </tr>);
+                    })}</tbody>
+            </table>}
         </>
     );
 }
@@ -48,7 +35,7 @@ const BookForm = (props) => {
     const update = (evt) => {
         const value = evt.target.value;
         const propertyName = evt.target.id;
-        setNewBook({...newBook,[propertyName]:value});
+        setNewBook({ ...newBook, [propertyName]: value });
     }
     const handleSubmit = (evt) => {
         evt.preventDefault();
@@ -56,25 +43,25 @@ const BookForm = (props) => {
         fetch('http://localhost:4000/books', {
             method: 'POST', // or 'PUT'
             headers: {
-              'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(newBook)
-          })
-          .then((res)=>res.json())
-          .then((data)=>{
-            console.log('DATA:', data);
-            props.isChanged(true);
-        });
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log('DATA:', data);
+                props.isChanged(true);
+            });
     }
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <input type="text" id="title" value={newBook.title} onChange={update} />
-            <input type="text" id="author" value={newBook.author} onChange={update} />
-            <input type="text" id="rating" value={newBook.rating} onChange={update} />
-            <input type="text" id="year_published" value={newBook.year_published} onChange={update} />
-            <input type="submit" value="Submit"/>
-        </form>
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="title" value={newBook.title} onChange={update} />
+                <input type="text" id="author" value={newBook.author} onChange={update} />
+                <input type="text" id="rating" value={newBook.rating} onChange={update} />
+                <input type="text" id="year_published" value={newBook.year_published} onChange={update} />
+                <input type="submit" value="Submit" />
+            </form>
         </>
     );
 };
